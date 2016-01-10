a01ca60c52542fc7b82f39fabb25d635bf9e2fe5
Eric Sciple <ersciple@microsoft.com>
2016-01-10 18:07:27 -0500

    .

--- Tests/L0/MSBuild/PassesArguments.ps1
+++ Tests/L0/MSBuild/PassesArguments.ps1
@@ -9,34 +9,31 @@ Register-Mock Format-MSBuildArguments { 'Some formatted arguments' } -- -MSBuild
 foreach ($clean in @($true, $false)) {
     foreach ($restoreNuGetPackages in @($true, $false)) {
         foreach ($logProjectEvents in @($true, $false)) {
-            foreach ($requireMSBuildVersion in @($true, $false)) {
-                Unregister-Mock Select-MSBuildLocation
-                Unregister-Mock Invoke-BuildTools
-                Register-Mock Select-MSBuildLocation { 'Some location' } -- -Method 'Some input method' -Location 'Some input location' -Version 'Some input version' -RequireVersion: $requireMSBuildVersion -Architecture 'Some input architecture'
-                Register-Mock Invoke-BuildTools { 'Some build output' }
+            Unregister-Mock Select-MSBuildLocation
+            Unregister-Mock Invoke-BuildTools
+            Register-Mock Select-MSBuildLocation { 'Some location' } -- -Method 'Some input method' -Location 'Some input location' -Version 'Some input version' -Architecture 'Some input architecture'
+            Register-Mock Invoke-BuildTools { 'Some build output' }
 
-                # Act.
-                $splat = @{
-                    'MSBuildLocationMethod' = 'Some input method'
-                    'MSBuildLocation' = 'Some input location' 
-                    'MSBuildArguments' = 'Some input arguments' 
-                    'Solution' = 'Some input solution' 
-                    'Platform' = 'Some input platform'
-                    'Configuration' = 'Some input configuration'
-                    'Clean' = $clean.ToString()
-                    'RestoreNuGetPackages' = $restoreNuGetPackages.ToString()
-                    'LogProjectEvents' = $logProjectEvents.ToString()
-                    'MSBuildVersion' = 'Some input version'
-                    'RequireMSBuildVersion' = $requireMSBuildVersion.ToString()
-                    'MSBuildArchitecture' = 'Some input architecture'
-                    'OmitDotSource' = 'true'
-                }
-                $output = & $PSScriptRoot\..\..\..\Tasks\MSBuild\MSBuild.ps1 @splat
-
-                # Assert.
-                Assert-AreEqual 'Some build output' $output
-                Assert-WasCalled Invoke-BuildTools -- -NuGetRestore: $restoreNuGetPackages -SolutionFiles @('Some solution 1', 'Some solution 2') -MSBuildLocation 'Some location' -MSBuildArguments 'Some formatted arguments' -Clean: $clean -NoTimelineLogger: $(!$logProjectEvents)
+            # Act.
+            $splat = @{
+                'MSBuildLocationMethod' = 'Some input method'
+                'MSBuildLocation' = 'Some input location' 
+                'MSBuildArguments' = 'Some input arguments' 
+                'Solution' = 'Some input solution' 
+                'Platform' = 'Some input platform'
+                'Configuration' = 'Some input configuration'
+                'Clean' = $clean.ToString()
+                'RestoreNuGetPackages' = $restoreNuGetPackages.ToString()
+                'LogProjectEvents' = $logProjectEvents.ToString()
+                'MSBuildVersion' = 'Some input version'
+                'MSBuildArchitecture' = 'Some input architecture'
+                'OmitDotSource' = 'true'
             }
+            $output = & $PSScriptRoot\..\..\..\Tasks\MSBuild\MSBuild.ps1 @splat
+
+            # Assert.
+            Assert-AreEqual 'Some build output' $output
+            Assert-WasCalled Invoke-BuildTools -- -NuGetRestore: $restoreNuGetPackages -SolutionFiles @('Some solution 1', 'Some solution 2') -MSBuildLocation 'Some location' -MSBuildArguments 'Some formatted arguments' -Clean: $clean -NoTimelineLogger: $(!$logProjectEvents)
         }
     }
 }
